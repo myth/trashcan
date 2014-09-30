eee5194390321a67fc6c7decfeaff9051d72fa91
Freboto <toernvall@gmail.com>
2014-09-30 15:32:00 +0200

    added get_startpoint in datastructures.py

--- tdt4136/astar/datastructures.py
+++ tdt4136/astar/datastructures.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 
-def make_matrix(board):
-    """
-    This function returns a matrix of characters based on an input string
-    """
+class Board:
+    def __init__(self, board):
+        self.matrix = self.make_matrix(board)
 
-    return [list(line) for line in board.split('\n')]
 
+    def make_matrix(self, board):
+        """
+        This function returns a matrix of characters based on an input string
+        """
+        return [list(line) for line in board.split('\n')]
+
+
+
+    def get_startpoint(self):
+        """
+        This function returns det x and y coordinates for the starting position A.
+        """
+        for x in self.matrix:
+            for y in self.matrix[x]:
+                if y == 'A':
+                    return (x,y)
