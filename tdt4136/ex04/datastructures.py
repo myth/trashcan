1a6120c26c61e4de85406c7f283eb1a0902b6ef7
mythern <myth@overflow.no>
2014-10-09 14:01:02 +0200

    Add some more stuff.. and a couple of test files

--- tdt4136/ex04/datastructures.py
+++ tdt4136/ex04/datastructures.py
@@ -43,12 +43,13 @@ class EggCarton(AbstractBoard):
         """
 
         cols = []
+        n = len(self.matrix)
 
-        for x in xrange(len(self.matrix[0])):
+        for x in xrange(n):
             colsum = 0
-            for y in xrange(len(self.matrix)):
+            for y in xrange(n):
                 colsum += self.matrix[y][x]
-            cols.append(colsum)
+            cols.append(self.K - colsum)
 
         return cols
 
@@ -56,7 +57,47 @@ class EggCarton(AbstractBoard):
         """
         Returns the delta of eggs in each diagonal compared to K
         """
-        return []
+        diags = []
+        n = len(self.matrix)
+
+        for i in xrange(n):
+            for j in xrange(n):
+                a = 1
+
+        return diags
+
+    def objective(self):
+        """
+        Returns a number between 0 and 1 that gives an indication of how close
+        this board representation is to an optimal solution
+        """
+
+        rows = self.check_rows()
+        cols = self.check_cols()
+        diags = self.check_diags()
+
+        concat = rows + cols + diags
+        total = 0
+        # Max potential horiz and vertical board check values
+        maximum = len(self.matrix) * self.K * 2
+        # Max total list sum for right and left diags
+        maximum += ((len(self.matrix) * 2 - 1) - ((self.K - 1) * 2)) * 2
+
+        for x in concat:
+            if x < 0:
+                return 0
+            total += x
+
+        print total
+        print maximum
+
+        if total == 0:
+            return 1
+
+        return 1.0 - (float(total) / float(maximum))
+
+    def __str__(self):
+        return repr(self.matrix) + '\n' + repr(self.check_rows()) + '\n' + repr(self.check_cols()) + '\nObjective value: %f' % self.objective()
         
 
 class PegBoard(AbstractBoard):
---
+++ tdt4136/ex04/test.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+
+import logging
+
+from datastructures import *
+
+e = EggCarton(5, 5, 2)
+
+print e
+print ""
+
+e.matrix[2][3] = 1
+e.matrix[3][4] = 1
+
+print e
+
+e.matrix = [
+    [1,0,1,0,0],
+    [0,1,0,0,1],
+    [0,1,0,1,0],
+    [1,0,0,0,1],
+    [0,0,1,1,0],
+]
+
+print e
