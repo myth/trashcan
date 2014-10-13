b5cfec29cf1e6a95e0b025f69d7fdb7654c7395b
Freboto <toernvall@gmail.com>
2014-10-13 14:09:51 +0200

    flere endringer i algorithms.py

--- tdt4136/ex04/algorithms.py
+++ tdt4136/ex04/algorithms.py
@@ -8,14 +8,14 @@ from random import random, randint
 
 def sa(board):
 
-    temp = 30*1000
+    temp = 30
     f_target = 1.0
     start = board
     current_f = start.objective()
     pmax = start
 
     while temp > 0:
-        print "this is temp: ", temp
+        #print "this is temp: ", temp
         t = time.time()
         if current_f >= f_target:
             return current_f
@@ -23,25 +23,31 @@ def sa(board):
         else:
             nabo = start.create_neighbors(4)
 
-            for x in nabo:
-                if x.objective() > pmax.objective():
-                    pmax = x
+            for y in nabo:
+                if y.objective() > pmax.objective():
+                    pmax = y
 
-            q = (pmax.objective()-current_f)/current_f
+            q = ((pmax.objective() - current_f)/current_f)
+            print "Dette er q: ", q
 
-            p = min(1.0, exp((q*-1)/temp))
+            p = min([1, exp((q*-1)/temp)])
+            print "Dette er p: ", p
+
             x = random()
+            print "Dette er x: ", x
 
             if x > p:
                 current_f = pmax.objective()
+                print "pmax"
 
             else:
                 current_f = nabo[randint(0, 3)]
                 current_f = current_f.objective()
+                print "Random nabo"
 
             temp -= time.time() - t
+            print "this is current_f", current_f
 
-egg = EggCarton(5, 5, 2)
+egg = EggCarton(8, 8, 1)
 egg.create_random_board()
-
 sa(egg)
