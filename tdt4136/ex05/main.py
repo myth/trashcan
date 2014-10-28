e60fc75cb48023ad61238f43b298c4ce9d7d095f
mythern <myth@overflow.no>
2014-10-28 21:50:11 +0100

    Now runs as main.py

--- tdt4136/ex05/main.py
+++ tdt4136/ex05/main.py
@@ -1,46 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-This is the main program file for the A* application
+# -*- encoding: utf-8 -*-
 
-Authors: Fredrik B. Tørnvall & Aleksander Skraastad
-"""
-import datetime
 import logging
+import time
+from tools import *
 
-from Tkinter import *
-
-from algorithms import *
-from view import Main
-
-
-def center_window(r):
-    """
-    Takes in a r widget, and positions the window to center of screen
-    :param r: Takes the root window widget as an argument
-    """
-
-    width = 1280
-    height = 600
-
-    screen_width = r.winfo_screenwidth()
-    screen_height = r.winfo_screenheight()
-
-    x = (screen_width - width) / 2
-    y = (screen_height - height) / 2
-
-    r.geometry('%dx%d+%d+%d' % (width, height, x, y))
-
-
-if __name__ == '__main__':
-    """
-    Main run method
-    """
-
-    logging.basicConfig(filename='debug.log', level=logging.DEBUG)
-    logging.debug('Starting program at %s' % datetime.datetime.utcnow().strftime('%H:%M:%S'))
-
-    root = Tk()
-    center_window(root)
-    app = Main(root)
-
-    root.mainloop()
+from datastructures import *
+e = create_sudoku_csp("sudokus/evil.txt")
+print_sudoku_solution(e.backtracking_search())
--- tdt4136/ex05/test2.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-import logging
-import time
-from tools import *
-
-from datastructures import *
-e = create_sudoku_csp("sudokus/evil.txt")
-print_sudoku_solution(e.backtracking_search())
