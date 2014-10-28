fb2bda83973d2c9d626c26a25c2456aa26fed3cc
mythern <myth@overflow.no>
2014-10-28 10:42:49 +0100

    Initial split

---
+++ tdt4136/ex05/datastructures.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the classes data representation
+"""
+import logging
+from copy import deepcopy
+
+
---
+++ tdt4136/ex05/test.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+
+import logging
+import time
+
+from datastructures import *
+
+e = EggCarton(10, 10, 3)
+e.create_random_board()
+
+t = time.time()
+
+print e
+
+temp_max = 0
+temp = e
+
+while True:
+
+    neighbors = temp.create_neighbors(6)
+    current = temp
+
+    for n in neighbors:
+        if n.objective() > temp_max:
+            temp = n
+            temp_max = n.objective()
+    
+    if temp is not current:
+        print "Found better neighbor... %f" % temp_max
+        print temp
+        
+
+    if temp.objective() == 1.0:
+        print temp
+        break
+
+print "Took %f seconds" % (time.time() - t)
