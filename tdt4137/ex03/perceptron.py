1de5ccfb97368df219b3eeeb775ce22584633fc1
mythern <myth@overflow.no>
2015-10-10 01:21:28 +0200

    Add mass testing

--- tdt4137/ex03/perceptron.py
+++ tdt4137/ex03/perceptron.py
@@ -3,13 +3,16 @@
 import random
 from numpy import append, array, dot
 
+MAX_EPOCHS = 100
+
+
+# Perceptron object
 
 class Perceptron(object):
-    def __init__(self, t=0.2, lrate=0.2, bias=1):
+    def __init__(self, n=2, t=1, lrate=0.2):
         self.theta = t
         self.learning_rate = 0.2
-        self.bias = array([1])
-        self.w = array([random.uniform(-0.5, 0.5) for _ in range(3)])
+        self.w = array([random.uniform(-0.5, 0.5) for _ in range(n)])
 
     def train(self, data):
         epoch = 0
@@ -17,30 +20,36 @@ class Perceptron(object):
             epoch += 1
             errors = 0
             for iv, ex in data:
-                result = dot(append(self.bias, iv), self.w) > self.theta
+                result = dot(iv, self.w) > self.theta
                 error = ex - result
                 if error != 0:
                     errors += 1
-                    self.w = array([self.w[i] + self.learning_rate * error * value for i, value in enumerate(append(self.bias, iv))])
+                    self.w = array([self.w[i] + self.learning_rate * error * value for i, value in enumerate(iv)])
                     print('Weight vector changed: %s' % repr(self.w))
-
-            if not errors:
+            if not errors or epoch > MAX_EPOCHS:
                 break
-
         print('Training complete in %d epochs' % epoch)
 
+# Support code
 
 def test(p, data):
+    errors = False
     for iv, ex in data:
-        result = dot(append(p.bias, iv), p.w) >= p.theta
+        result = dot(iv, p.w) > p.theta
         error = ex - result
         if error != 0:
             print('Classification error on %s with output %d' % (repr(iv), ex))
+            errors = True
         else:
             print('Success! on %s with output %d' % (repr(iv), ex))
+    return not errors
 
 
 if __name__ == '__main__':
+
+    print('[i] --- PERCEPTRON v1.0 ----------------------')
+
+    TOTAL_TRAINING_SESSIONS = 500
     AND = [
         (array([0, 0]), 0),
         (array([0, 1]), 0),
@@ -54,15 +63,25 @@ if __name__ == '__main__':
         (array([1, 1]), 1),
     ]
 
-    print('--- Training AND ---------------------')
-    p_and = Perceptron(t=0.2, lrate=0.2)
-    p_and.train(AND)
-    print('--- Training OR --------------------- ')
-    p_or = Perceptron(t=0.2, lrate=0.2)
-    p_or.train(OR)
+    trains = 0
+    and_errors = 0
+    or_errors = 0
+    while trains < TOTAL_TRAINING_SESSIONS:
+        trains += 1
+        print('[%d] --- Training AND ---------------------' % trains)
+        p_and = Perceptron(t=0.4, lrate=0.1)
+        p_and.train(AND)
+        print('[%d] --- Training OR --------------------- ' % trains)
+        p_or = Perceptron(t=0.0, lrate=0.2)
+        p_or.train(OR)
 
-    print('--- Testing AND ----------------------')
-    test(p_and, AND)
-    print('--- Testing OR -----------------------')
-    test(p_or, OR)
+        print('[%d] --- Testing AND ----------------------' % trains)
+        if not test(p_and, AND):
+            and_errors += 1
+        print('[%d] --- Testing OR -----------------------' % trains)
+        if not test(p_or, OR):
+            or_errors += 1
+
+
+    print('[i] Mass train and test complete (%d iterations). Errors: AND: %d OR: %d' % (trains, and_errors, or_errors))
 
