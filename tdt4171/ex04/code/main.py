b48a465d018a95ab458d13efd2b4bced1edf5a05
myth <myth@overflow.no>
2016-04-21 03:05:37 +0200

    Working version

--- tdt4171/ex04/code/main.py
+++ tdt4171/ex04/code/main.py
@@ -1,194 +1,107 @@
 # -*- coding=utf-8 -*-
 
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-import networkx as nx
 import math
-from random import randint, random
+import random
 
 
-class Node:
+class Node(object):
     """
-    Node object. You know, like trees
+    Tree node
     """
 
-    def __init__(self, label, i=0):
+    def __init__(self, label):
         self.label = label
         self.children = {}
-        self.ID = i
 
     def __str__(self):
         """
-        String representation of this node
-        :return: This node and its children as a string
+        String representation of the tree from this node as root node
+        :return: A PHP SyntaxTree string
         """
 
-        return 'Node[%d] Label: %d' % (self.ID, self.label)
-
-    def __repr__(self):
-        """
-        Representational form of this Npde
-        :return: Delegates to __str__
-        """
-
-        return self.__str__()
+        if self.children:
+            return '[%s ' % self.label + '%s]' % ''.join(str(self.children[key]) for key in self.children.keys())
+        else:
+            return '[%s]' % self.label
 
 
-class DecisionTreeLearning(object):
+class DecisionTreeLearner(object):
     """
-    Implementation class for DecisionTreeLearning
+    Implementation Class for Decision Tree learning algorithm
     """
 
     def __init__(self):
+        """
+        Constructor
+        """
+
         self.training = get_data()
         self.testing = get_data(training=False)
+        self.root = None
         self.random = False
-        self.tree = None
-        self.id_counter = 0
 
-    def classify(self, obj):
+    def classify(self, example):
         """
-        Attempts to classify an object
-        :param obj: An example attribute vector
-        :return: The class which the object most likely has
+        Attempt to classify an example
+        :param example: An attribute vector
+        :return: A class
         """
 
-        root = self.tree
+        root = self.root
         while root.children:
-            root = root.children[obj[root.label]]
+                root = root.children[example[root.label]]
 
         return root.label
 
     def test(self):
         """
-        Perform a complete test on the entire test set.
-        :return: The correctness score of this test
+        Test on the test set
         """
-        total_correct = 0
-        for example in self.testing:
-            result = self.classify(example)
-            correct = result == example[-1]
-            if correct:
-                total_correct += 1
-            print('Classifying %s ... Result: %d, Correct: %s' % (example, result, correct))
 
-        return total_correct / len(self.testing)
+        return float(sum(map(lambda x: x[-1] == self.classify(x), self.testing))) / len(self.testing)
 
     def train(self):
         """
-        Train this decision tree
+        Train on the training set
+        :return: A tree root node
         """
 
-        self.tree = self._dtl(self.training, list(range(0, 7)), list())
+        self.root = self._dtl(self.training, list(range(7)), list())
 
-        return self.tree
-
-    @staticmethod
-    def _check_classifiability(examples):
-        """
-        Checks if all objects are of same class in example set
-        :param examples: A list of example pbjects
-        :return: True of all are same class, False otherwise
-        """
-
-        tot = sum((o[-1] for o in examples))
-
-        return tot == 0 or tot == len(examples)
+        return self.root
 
     def _dtl(self, examples, attributes, parent_examples):
         """
-        Internal algorithm implementation
-        :param examples: Example set
-        :param attributes: Attribute vector
-        :param parent_examples: Parent level example superset
-        :return: A tree
+        Private helper that performs the recursion
+        :param examples:
+        :param attributes:
+        :param parent_examples:
+        :return:
         """
-
-        self.id_counter += 1
 
         if not examples:
-            return Node(plurality_value(parent_examples), self.id_counter)
-        elif self._check_classifiability(examples):
-            return Node(examples[0][-1], self.id_counter)
-        elif sum(attributes) == 0:
-            return Node(plurality_value(examples), self.id_counter)
+            return Node(plurality_value(parent_examples))
+        elif is_classifiable(examples):
+            return Node(examples[0][-1])
+        elif not attributes:
+            return Node(plurality_value(examples))
         else:
-            a = self._importance(examples, attributes)
-            tree = Node(a, self.id_counter)
+            if self.random:
+                a = random.choice(attributes[:-1])
+            else:
+                a = importance(examples, attributes)
 
-            attrs = attributes[:]
-            attrs.pop(a)
+            tree = Node(a)
+            attributes.remove(a)
 
-            for i in range(2):
-                subset_examples = list(filter(lambda x: x[a] == i, examples))
-                subtree = self._dtl(subset_examples, attrs, examples)
-                tree.children[i] = subtree
-
+            for n in range(2):
+                liste = []
+                for e in examples:
+                    if e[a] == n:
+                        liste.append(e)
+                sub_tree = self._dtl(liste, list(attributes), examples)
+                tree.children[n] = sub_tree
             return tree
-
-    def _importance(self, examples, attributes):
-        """
-        Calculate importance from examples and attributes
-        :param examples: A list of example objects
-        :param attributes: An attribute vector
-        :return: The index of the most important attribute
-        """
-
-        if self.random:
-            return argmax(0 if a == 0 else random() for a in attributes)
-        else:
-            return argmax(importance(x, examples) for x in attributes)
-
-    def __str__(self):
-        """
-        String representation of this DecisionTreeLearner
-        :return: A string
-        """
-
-        output = '--- Decision Tree ---------\n'
-        stack = [(self.tree, 0)]
-        while stack:
-            n, i = stack.pop()
-            output += '\t' * i + '- %s\n' % n
-
-            if n.children:
-                for x in n.children.values():
-                    stack.append((x, i+1))
-
-        return output
-
-
-def argmax(sequence):
-    """
-    Returns the index of the highest valued element in the sequence
-    :param sequence: An iterable object
-    :return: An integer representing the highest value position in the sequence
-    """
-
-    top = 0
-    best = -float('Inf')
-    for i, x in enumerate(sequence):
-        if x > best:
-            top = i
-            best = x
-
-    return top
-
-
-def b(q):
-    """
-    Calculate the B value from the book
-    :param q: The probability of q
-    :return:
-    """
-
-    if not q:
-        return 0
-    if q == 1:
-        return 1
-
-    return -(q * math.log2(q) + (1 - q) * math.log2(1 - q))
 
 
 def get_data(training=True):
@@ -207,36 +120,6 @@ def get_data(training=True):
         return [list(map(lambda x: int(x) - 1, line.strip().split('\t'))) for line in f]
 
 
-def importance(a, examples):
-    """
-    Calculate the importance of attribute A, given an example set
-    :param examples: A list of example objects
-    :param a: An attribute
-    :return: A list of importance values
-    """
-
-    if a == 0:
-        return 0
-
-    tot = len(examples)
-    tot_pos = sum(i[-1] for i in examples)
-    tot_neg = tot - tot_pos
-
-    j = list(filter(lambda x: x[a], examples))
-    k = list(filter(lambda x: not x[a], examples))
-
-    total_entropy = 0
-    for obj in (j, k):
-        p = len(list(filter(lambda o: o[-1], obj)))
-        n = len(list(filter(lambda o: not o[-1], obj)))
-        if p == 0 and n == 0:
-            return 0
-        e = (p + n) / (tot_pos + tot_neg)
-        total_entropy += e * b(p / (p + n))
-
-    return b(tot_pos / (tot_pos + tot_neg)) - total_entropy
-
-
 def plurality_value(examples):
     """
     Tie breaker function
@@ -252,76 +135,67 @@ def plurality_value(examples):
     elif neg > pos:
         return 0
     else:
-        return randint(0, 1)
+        return random.randint(0, 1)
 
 
-def hierarchy_pos(G, root, width=1.0, vert_gap=0.05, vert_loc=0, xcenter=0.2, pos=None, parent=None):
+def is_classifiable(examples):
     """
-    If there is a cycle that is reachable from root, then this will see infinite recursion.
+    Check whether or not a set of examples all are of the same class
+    :param examples: A list of attribute vectors
+    :return: True if classified, False otherwise
     """
-    if pos is None:
-        pos = {root: (xcenter, vert_loc)}
+
+    tot = sum(x[-1] for x in examples)
+
+    if tot == 0 or tot == len(examples):
+        return True
+    return False
+
+
+def b(q):
+    """
+    The B function
+    :param q: The input probability
+    :return: The B value
+    """
+
+    if q == 0:
+        return q
     else:
-        pos[root] = (xcenter, vert_loc)
-    neighbors = G.neighbors(root)
-    if parent is not None:
-        neighbors.remove(parent)
-    if len(neighbors) != 0:
-        dx = width / len(neighbors)
-        nextx = xcenter - width / 2 - dx / 2
-    for neighbor in neighbors:
-        nextx += dx
-        pos = hierarchy_pos(
-            G, neighbor, width=dx, vert_gap=vert_gap,
-            vert_loc=vert_loc - vert_gap, xcenter=nextx, pos=pos,
-            parent=root
-        )
-    return pos
+        return -(q * math.log(q, 2) + (1.0 - q) * math.log((1.0 - q), 2))
+
+
+def importance(examples, attributes):
+    """
+    Analyze which attribute is of highest importance in order to create a good split
+    :param examples: The example set
+    :param attributes: The attribute vector
+    :return: An attribute index (vector position)
+    """
+
+    values = {a: b(sum(x[a] == examples[0][a] for x in examples) / len(examples)) for a in attributes}
+
+    index, val = min(values.items())
+
+    return index
 
 
 def main():
     """
-    Main method
+    Main application method
     """
 
-    dtl = DecisionTreeLearning()
-    print('Random Importance')
+    dtl = DecisionTreeLearner()
     dtl.random = True
-    dtl.train()
-    print(dtl)
-    score = dtl.test()
+    for x in range(5):
+        dtl.train()
+        print('Score: %.2f' % (dtl.test() * 100))
+    print(dtl.root)
 
-    print('\nRandom test score: %.2f\n' % score)
-
-    g = nx.Graph()
-    q = [dtl.tree]
-    while q:
-        n = q.pop()
-        for c in n.children.values():
-            q.append(c)
-            g.add_edge(n.ID, c.ID)
-
-    nx.draw(g, pos=hierarchy_pos(g, dtl.tree.ID))
-    plt.show()
-
-    print('Entropy Importance')
     dtl.random = False
-    dtl.train()
-    print(dtl)
-    score = dtl.test()
+    for x in range(5):
+        dtl.train()
+        print('Score: %.2f' % (dtl.test() * 100))
+    print(dtl.root)
 
-    print('\nInformation Gain test score: %.2f\n' % score)
-
-    g = nx.Graph()
-    q = [dtl.tree]
-    while q:
-        n = q.pop()
-        for c in n.children.values():
-            q.append(c)
-            g.add_edge(n.ID, c.ID)
-
-    nx.draw(g, pos=hierarchy_pos(g, dtl.tree.ID))
-    plt.show()
-
-if __name__ == "__main__":
-    main()
+main()
